fb7494753412fe6cf62008e16872aabd8b624709
Jenn Hau <wongjennhau@gmail.com>
2022-12-23 14:39:56 +0800

    restore run file

---
+++ src/pms_parkhill_residence/PMS_Parkhill_Residence.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pms_parkhill_residence;
+
+/**
+ *
+ * @author wongj
+ */
+public class PMS_Parkhill_Residence {
+    
+    public static void main(String[] args) {
+        // call the main method of Home Screen
+        HomePage.main(new String[0]);
+    }
+}
