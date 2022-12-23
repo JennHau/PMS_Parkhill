6f0d61b48bcf6c7c2a3c81ac3196b529d3a643a0
Jenn Hau <wongjennhau@gmail.com>
2022-12-23 14:48:16 +0800

    Users class getter and setter done

--- src/pms_parkhill_residence/Users.java
+++ src/pms_parkhill_residence/Users.java
@@ -52,4 +52,144 @@ public class Users {
         this.birthOfDate = birthOfDate;
         this.unitNo = unitNo;
     }
+
+    /**
+     * @return the userID
+     */
+    public String getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the phoneNo
+     */
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    /**
+     * @param phoneNo the phoneNo to set
+     */
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    /**
+     * @return the identificationNo
+     */
+    public String getIdentificationNo() {
+        return identificationNo;
+    }
+
+    /**
+     * @param identificationNo the identificationNo to set
+     */
+    public void setIdentificationNo(String identificationNo) {
+        this.identificationNo = identificationNo;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the birthOfDate
+     */
+    public Date getBirthOfDate() {
+        return birthOfDate;
+    }
+
+    /**
+     * @param birthOfDate the birthOfDate to set
+     */
+    public void setBirthOfDate(Date birthOfDate) {
+        this.birthOfDate = birthOfDate;
+    }
+
+    /**
+     * @return the unitNo
+     */
+    public String getUnitNo() {
+        return unitNo;
+    }
+
+    /**
+     * @param unitNo the unitNo to set
+     */
+    public void setUnitNo(String unitNo) {
+        this.unitNo = unitNo;
+    }
 }
