d1020748da991809262bbb65489bc41678bc13f7
Yao Qian <yaoqianlow89@gmail.com>
2022-12-23 13:51:08 +0800

    1111

--- src/pms_parkhill_residence/aboutProperty.java
+++ src/pms_parkhill_residence/aboutProperty.java
@@ -11,12 +11,12 @@ import java.awt.Toolkit;
  *
  * @author wongj
  */
-public class AboutProperty extends javax.swing.JFrame {
+public class aboutProperty extends javax.swing.JFrame {
 
     /**
      * Creates new form homePage
      */
-    public AboutProperty() {
+    public aboutProperty() {
         initComponents();
         setWindowIcon();
     }
@@ -472,14 +472,18 @@ public class AboutProperty extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(aboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(aboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(aboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(aboutProperty.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -488,7 +492,7 @@ public class AboutProperty extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AboutProperty().setVisible(true);
+                new aboutProperty().setVisible(true);
             }
         });
     }
